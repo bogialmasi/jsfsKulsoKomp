1bef99579fe60f715f4655ee0c7a302ad1246abf
bogialmasi <boglarkaalmasi@gmail.com>
2022-02-07 18:58:19 +0100

    Fisher getter + setter a Lake-ben

--- src/main/java/lsz/KulsoKomp/components/Lake.java
+++ src/main/java/lsz/KulsoKomp/components/Lake.java
@@ -4,14 +4,27 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 public class Lake {
     private Fish fish;
+    private Fisher fisher;
 
+    /* Fish */
     @Autowired
-    public void setFish(Fish fish){
+    public void setFish(Fish fish) {
         this.fish = fish;
     }
 
     @Autowired
     public Fish getFish() {
         return fish;
+    }
+
+    /* Fisher */
+    @Autowired
+    public void setFisher(Fisher fisher) {
+        this.fisher = fisher;
+    }
+
+    @Autowired
+    public Fisher getFisher() {
+        return fisher;
     }
 }
