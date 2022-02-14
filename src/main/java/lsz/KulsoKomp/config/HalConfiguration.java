d5762d1dd9aa250fb0be9bd255090243e0b4ef07
bogialmasi <boglarkaalmasi@gmail.com>
2022-02-14 18:12:34 +0100

    HalConfiguration, HalProperty

--- src/main/java/lsz/KulsoKomp/config/HalConfiguration.java
+++ src/main/java/lsz/KulsoKomp/config/HalConfiguration.java
@@ -1,14 +1,24 @@
 package lsz.KulsoKomp.config;
 
 import lsz.KulsoKomp.components.Hal;
+import lsz.KulsoKomp.properties.HalProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@EnableConfigurationProperties({HalProperty.class}) /* endegélyezi/feldolgozza a HalProperty-t, amit fel kell venni adattagként */
 public class HalConfiguration {
 
+    public final HalProperty halProperty;
+
+    public HalConfiguration(HalProperty halProperty) {
+        this.halProperty = halProperty;
+        System.out.println(halProperty.getSize());
+    }
+
     @Bean
-    public Hal fish (){
+    public Hal hal (){
         return new Hal(2);
     }
 }
--- src/main/java/lsz/KulsoKomp/properties/HalProperty.java
+++ src/main/java/lsz/KulsoKomp/properties/HalProperty.java
@@ -4,10 +4,11 @@ import com.google.common.reflect.ClassPath;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.PropertySource;
 
 @PropertySource("classpath:application.properties")
-@ConfigurationProperties(prefix = "fish")
+@ConfigurationProperties(prefix = "hal")
 public class HalProperty {
     private @Getter @Setter Integer size;
 }
